2934465483265bc651419cb437eb714c51adb61c
Dinesh Budhathoki <physmarika@gmail.com>
2026-03-10 15:48:16 +0800

    practicing finding missing number

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
 //this is for reversing the word
-let str = "dinesh budhathoki";
-const reverseString = (str) => {
-  return str.split(" ").reverse().join(" ");
-};
-console.log(reverseString(str));
+// let str = "dinesh budhathoki";
+// const reverseString = (str) => {
+//   return str.split(" ").reverse().join(" ");
+// };
+// console.log(reverseString(str));
+
+//this is for checking the palindrome
+// let forward = "madam";
+// const isPalnidrome = () => {
+//   let reversed = forward.split("").reverse().join("");
+//   return forward === reversed;
+// };
+// console.log(isPalnidrome(forward));
+
+//this is for finding the maximum number
+
+// const findMax = (num) => {
+//   return Math.min(...num);
+// };
+// console.log(findMax(num));
+
+//this is for finding the sum of the array
+// const arr = [3, 10, 20, 50, 51];
+
+// const sumArray = (arr) => {
+//   return arr.reduce((sum, n) => sum + n, 0);
+// };
+// console.log(sumArray(arr));
+
+//this is for remove duplicate
+// const dubs = [1, 2, 2, 3, 2, 3, 4];
+// const removeDubs = (dubs) => {
+//   return [...new Set(dubs)];
+// };
+// console.log(removeDubs(dubs));
+
+//this is for find the missing number
+// const number = [1, 2, 3, 5];
+// const num = [1, 2, 3, 5];
+// const missingNum = (num) => {
+//   let n = num.length + 1;
+//   let expectedSum = (n * (n + 1)) / 2;
+//   let actualSum = num.reduce((sum, n) => sum + n, 0);
+//   return expectedSum - actualSum;
+// };
+// console.log(missingNum(num));
