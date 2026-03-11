485cdce4df797961db284defc23b5c08675f0c66
Dinesh Budhathoki <physmarika@gmail.com>
2026-03-11 15:20:08 +0800

    learning merging arrays

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//this is for reversing the word
+//1. this is for reversing the word
 // let str = "dinesh budhathoki";
 // const reverseString = (str) => {
 //   return str.split(" ").reverse().join(" ");
 // };
 // console.log(reverseString(str));
 
-//this is for checking the palindrome
+//2. this is for checking the palindrome
 // let forward = "madam";
 // const isPalnidrome = () => {
 //   let reversed = forward.split("").reverse().join("");
@@ -13,14 +13,13 @@
 // };
 // console.log(isPalnidrome(forward));
 
-//this is for finding the maximum number
-
+//3. this is for finding the maximum number
 // const findMax = (num) => {
 //   return Math.min(...num);
 // };
 // console.log(findMax(num));
 
-//this is for finding the sum of the array
+//4. this is for finding the sum of the array
 // const arr = [3, 10, 20, 50, 51];
 
 // const sumArray = (arr) => {
@@ -28,14 +27,14 @@
 // };
 // console.log(sumArray(arr));
 
-//this is for remove duplicate
+//5. this is for remove duplicate
 // const dubs = [1, 2, 2, 3, 2, 3, 4];
 // const removeDubs = (dubs) => {
 //   return [...new Set(dubs)];
 // };
 // console.log(removeDubs(dubs));
 
-//this is for find the missing number
+//6. this is for find the missing number
 // const number = [1, 2, 3, 5];
 // const num = [1, 2, 3, 5];
 // const missingNum = (num) => {
@@ -46,11 +45,11 @@
 // };
 // console.log(missingNum(num));
 
+//7. this is for vawel count
 // const str = "dinesh";
 // const vowelCount = (str) => {
 //   let vowel = "aeiou";
 //   let count = 0;
-
 //   for (let char of str.toLowerCase()) {
 //     if (vowel.includes(char)) count++;
 //   }
@@ -58,7 +57,7 @@
 // };
 // console.log(vowelCount(str));
 
-//this is for finding the second largest number
+//8. this is for finding the second largest number
 // const num = [10, 20, 5, 8];
 // const secondLarge = (num) => {
 //   num = [...new Set(num)];
@@ -67,7 +66,7 @@
 // };
 // console.log(secondLarge(num));
 
-//this is for capitalizing the first letter
+//9. this is for capitalizing the first letter
 // const str = "dinesh budhathoki";
 // const capitalize = (str) => {
 //   return str
@@ -77,16 +76,32 @@
 // };
 // console.log(capitalize(str));
 
-//this is for finding the index in the array
+//10. this is for finding the index in the array
 // const arr = [5, 10, 30, 15];
 // const findIndex = (arr, target) => {
 //   return arr.indexOf(target);
 // };
 // console.log(findIndex(arr, 5));
 
-//this is for the finding the value of index
-const arr = [5, 10, 30, 15];
-const getValueAtIndex = (arr, index) => {
-  return arr[index];
+//11. this is for the finding the value of index
+// const arr = [5, 10, 30, 15];
+// const getValueAtIndex = (arr, index) => {
+//   return arr[index];
+// };
+// console.log(getValueAtIndex(arr, 3));
+
+//12. this is for flattn the array
+// const arr = [1, [2, 3], [4]];
+// const flatten = (arr) => {
+//   return arr.flat();
+// };
+// console.log(flatten(arr));
+
+//13.this is for rotating the array
+const arr = [1, 2, 3, 4];
+const rotate = (arr) => {
+  let last = arr.pop();
+  arr.unshift(last);
+  return arr;
 };
-console.log(getValueAtIndex(arr, 3));
+console.log(rotate(arr));
