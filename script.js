6d3c3876ef4df291d2a448eaec8bbcc087c6cc8e
Dinesh Budhathoki <physmarika@gmail.com>
2026-03-11 09:34:39 +0800

    learning find index of the element

--- script.js
+++ script.js
@@ -45,3 +45,48 @@
 //   return expectedSum - actualSum;
 // };
 // console.log(missingNum(num));
+
+// const str = "dinesh";
+// const vowelCount = (str) => {
+//   let vowel = "aeiou";
+//   let count = 0;
+
+//   for (let char of str.toLowerCase()) {
+//     if (vowel.includes(char)) count++;
+//   }
+//   return count;
+// };
+// console.log(vowelCount(str));
+
+//this is for finding the second largest number
+// const num = [10, 20, 5, 8];
+// const secondLarge = (num) => {
+//   num = [...new Set(num)];
+//   num.sort((a, b) => b - a);
+//   return num;
+// };
+// console.log(secondLarge(num));
+
+//this is for capitalizing the first letter
+// const str = "dinesh budhathoki";
+// const capitalize = (str) => {
+//   return str
+//     .split(" ")
+//     .map((w) => w[0].toUpperCase() + w.slice(1))
+//     .join(" ");
+// };
+// console.log(capitalize(str));
+
+//this is for finding the index in the array
+// const arr = [5, 10, 30, 15];
+// const findIndex = (arr, target) => {
+//   return arr.indexOf(target);
+// };
+// console.log(findIndex(arr, 5));
+
+//this is for the finding the value of index
+const arr = [5, 10, 30, 15];
+const getValueAtIndex = (arr, index) => {
+  return arr[index];
+};
+console.log(getValueAtIndex(arr, 3));
